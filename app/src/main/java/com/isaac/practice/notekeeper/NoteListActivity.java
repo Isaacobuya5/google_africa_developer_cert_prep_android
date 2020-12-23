c37b36069cb643ccb09d5faa50cbfb1e3e3aa0e6
Isaac Obuya <Szack4477@gmail.com>
2020-12-23 12:30:25 +0300

    implement item selection within list view, navigate to note activity using intent

--- app/src/main/java/com/isaac/practice/notekeeper/NoteListActivity.java
+++ app/src/main/java/com/isaac/practice/notekeeper/NoteListActivity.java
@@ -1,5 +1,6 @@
 package com.isaac.practice.notekeeper;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -43,5 +45,16 @@ public class NoteListActivity extends AppCompatActivity {
         // adapter
         ArrayAdapter<NoteInfo> adapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
         listNotes.setAdapter(adapterNotes);
+
+        // handle user selection - i.e. click on a single item within a list
+        // accepts an interface that is to be implemented by this Activity or create another class, inner class  or implement as anonymous class as shown below
+        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // launch note activity
+                Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
