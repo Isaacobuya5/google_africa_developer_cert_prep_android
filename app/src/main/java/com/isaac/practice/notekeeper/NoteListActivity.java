1cb82602e1b33f938401b462b9e82eeef6a91cdc
Isaac Obuya <Szack4477@gmail.com>
2021-01-14 11:10:44 +0300

    intro to recyclerview and steps involved

--- app/src/main/java/com/isaac/practice/notekeeper/NoteListActivity.java
+++ app/src/main/java/com/isaac/practice/notekeeper/NoteListActivity.java
@@ -97,4 +97,46 @@ public class NoteListActivity extends AppCompatActivity {
             }
         });
     }
+
+    /**
+     * RECYCLERVIEW
+     * Initially ListView was used to display a list of items but they had some limitations such as;
+     * -> Difficult to customize.
+     * -> Performance changes in some cases.
+     * -> Always displays vertical lists.
+     *
+     * But with RecyclerView, list display is divided into distinct phases and each phase provides a chance to customize.
+     * -> RecyclerView provides efficient display management.
+     *
+     * STEPS
+     * a. Associate LayoutManager with RecyclerView.
+     * -> Layout Manager is responsible for how the individual items are arranged.
+     * b. Associate an adapter with the recyclerview.
+     * -> The adapter is backed by some data. It is also responsible for creating individual view instances.
+     * -> Adapter takes the View instances and places it in the RecyclerView.
+     * -> Adapter creates additional view instances to allow display of additional rows in the recyclerview.
+     * -> * Adapter is also responsible to take that data and then load it into View items within RecyclerView.
+     *
+     * DEVELOPING RECYCLERVIEW COMPONENTS
+     * a. Design the RecyclerView as part of a layout resource.
+     * b. Create and associate a layout manager.
+     * c. Design the item view i.e. individual item as a layout resource.
+     * d. Create and associate an adapter. -> 3 functions i.e.
+     * -> Constructs item view instances.
+     * -> Manages data interaction.
+     * -> Associate data items with item views.
+     *
+     * LAYOUT MANAGERS
+     * RecyclerView.LayoutManagers is the base class.
+     * We can extend it to create custom layout managers but no need since Android already provides several implementations
+     * which caters for most needs.
+     * Examples;
+     * LinearLayoutManager - similar to ListView but with more features.
+     * GridLayoutManager - items are organized as a grid.
+     * We can specify span i.e. columns for vertical orientation.
+     * Adjacent items are consistently sized.
+     * StaggeredGridLayoutManager - items are organized as a grid but each item is consistently sized.
+     *
+     *
+     */
 }
